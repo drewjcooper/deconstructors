292dd48039e2fe3e600252c52636599f97aa07e3
Andrew Cooper <drew.j.cooper@gmail.com>
2018-07-16 12:09:12 +1000

    Factor out guard clauses

--- src/ArrayDeconstructors.cs
+++ src/ArrayDeconstructors.cs
@@ -6,35 +6,38 @@ namespace Extensions.Generic.Deconstructors
     {
         public static void Deconstruct<T>(this T[] array, out T item1, out T item2)
         {
-            if (array == null)
-            {
-                throw new ArgumentNullException(nameof(array));
-            }
-
-            if (array.Length < 2)
-            {
-                throw new ArgumentException("The provided array must have at least 2 elements.", nameof(array));
-            }
+            EnsureNotNull(array, nameof(array));
+            EnsureMinimumLength(array, 2, nameof(array));
 
             item1 = array[0];
             item2 = array[1];
-        } 
+        }
+
+        private static void EnsureMinimumLength<T>(T[] array, int minLength, string paramName)
+        {
+            if (array.Length < minLength)
+            {
+                throw new ArgumentException(
+                    $"The provided array must have at least {minLength} elements.", paramName);
+            }
+        }
 
         public static void Deconstruct<T>(this T[] array, out T item1, out T item2, out T item3)
         {
-            if (array == null)
-            {
-                throw new ArgumentNullException(nameof(array));
-            }
-
-            if (array.Length < 3)
-            {
-                throw new ArgumentException("The provided array must have at least 3 elements.", nameof(array));
-            }
+            EnsureNotNull(array, nameof(array));
+            EnsureMinimumLength(array, 3, nameof(array));
 
             item1 = array[0];
             item2 = array[1];
             item3 = array[2];
+        }
+
+        private static void EnsureNotNull(object obj, string paramName)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
         }
     }
 }
